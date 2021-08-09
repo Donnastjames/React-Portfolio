4abf17e55c2d7a424a45998ecbb3e8f4f3e0d596
Donna <donnastjames@comcast.net>
2021-08-08 22:08:36 -0700

    Removed references to the Home tab and updated the AboutMe page.  Added styles folder/Header.css

---
+++ src/components/pages/AboutMe.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function AboutMe() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        I grew up in California where I studied and received an associates degree in Economics.  I enjoyed working professionally as a Claims Auditor, and eventually as a Business Systems Analyst at Regence Blue Shield.  I got married and took care of my son, my pride and joy.  As my son grows more independent, I have more time to study and get back into the workforce.
+      </p>
+      <p>
+        I am currently working as a Legal Assistant for a lawfirm in downtown Seattle.  Starting with a few text books, I took an interest in web development, and then resolved to attain certification. I have always been detail oriented, and I enjoy a good challenge.  I take pride in my work, and always strive to do better in everything that I do. 
+      </p>
+    </div>
+  );
+}
