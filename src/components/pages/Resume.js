c6d30638cbb329c38910d6d5e2abd2f9f423f40d
Donna <donnastjames@comcast.net>
2021-08-12 18:33:21 -0700

    Added beginnings of Resume.js

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+
+const styles = {
+  sectionStyles: {
+    fontFamily: 'Georgia, Times New Roman, Times, serif',
+  },
+};
 
 export default function Resume() {
+  // https://stackoverflow.com/questions/66588340/custom-hook-for-window-resize
+  const [windowHeight, setHeight] = useState(window.innerHeight);
+  const [windowWidth, setWidth] = useState(window.innerWidth);
+  const ref = useRef(null);
+
   return (
-    <div style={{ position: 'fixed', top: '165px', height: '340px', overflow: 'auto' }}>
-      <h1>Resume Page</h1>
-      <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
-      </p>
+    <div 
+      ref={ref}
+      style={{ 
+        position: 'fixed', 
+        top: '200px',
+        left: '30px',
+        width: `${windowWidth - 45}px`,
+        height: `${windowHeight - 300}px`, 
+        overflow: 'auto' 
+      }}
+    >
+      <h1 style={styles.sectionStyles}>Resume</h1>
+        <h4 style={styles.sectionStyles}>Download my resume</h4>
+        <h3 style={styles.sectionStyles}>Front-end Proficiencies</h3> 
+            <ul style={styles.sectionStyles}>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>jQuery</li>
+              <li>React</li>
+              <li>Bootstrap</li>
+              <li>UIKit</li>
+              <li>Handlebars</li>
+            </ul>
+          <h3 style={styles.sectionStyles}>Back-end Proficiencies</h3>
+            <ul style={styles.sectionStyles}>
+              <li>APIs</li>
+              <li>Node</li>
+              <li>Express</li>
+              <li>MySQL, Sequelize</li>
+              <li>NoSQL</li>
+              <li>MongoDB, Mongoose</li>
+              <li>GrapghQL</li>
+              <li>Redux</li>
+          </ul>
     </div>
   );
 }
